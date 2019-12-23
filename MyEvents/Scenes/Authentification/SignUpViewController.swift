afd9b582a3408dbc1f83b99b5ba303ce47a62c0d
oualich <ouali.cherikh@gmail.Com>
2019-12-23 12:36:06 +0100

    Sign Up

--- MyEvents/Scenes/Authentification/SignUpViewController.swift
+++ MyEvents/Scenes/Authentification/SignUpViewController.swift
@@ -9,12 +9,66 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    //Outlet
+    @IBOutlet weak var SignUpButton: UIButton!
+    
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    //Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupButton()
         // Do any additional setup after loading the view.
     }
-
-
+    // private function
+    private func setupButton() {
+        SignUpButton.layer.cornerRadius = 20
+        LoginButton.layer.cornerRadius = 20
+        LoginButton.layer.borderWidth = 3
+        LoginButton.layer.borderColor = UIColor.red.cgColor
+    }
+    private func setupTextFieldManager() {
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        emailTextField.delegate = self
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(tapGesture)
+    }
+    // Action
+    @objc private func hideKeyboard(){
+        usernameTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        emailTextField.resignFirstResponder()
+    }
+    
+    @IBAction func touchToSignUp(_ sender: Any) {
+        if usernameTextField.text != "" && emailTextField.text != "" && passwordTextField.text != "" {
+            print("username : \(usernameTextField.text ?? "jon")")
+            print("password : \(passwordTextField.text ?? "doe")")
+            print("email : \(emailTextField.text ?? "jondoe@gmail.com")")
+        }
+    }
+    
+    @IBAction func touchToLogin(_ sender: Any) {
+        guard let navigationView = self.navigationController?.view else {
+            return
+        }
+        UIView.transition(with: navigationView, duration: 0.5, options: .transitionFlipFromTop, animations: {
+        self.navigationController?.pushViewController(LoginViewController(), animated: true)
+        })
+    }
+    
+    
+}
+extension SignUpViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
 }
--- Podfile
+++ Podfile
@@ -1,10 +1,12 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-target 'Geoloc' do
+target 'MyEvents' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for Geoloc
-  pod 'Alamofire', '~> 5.0.0-rc.3'
+  # Pods for MyEvents
+    pod 'Firebase/Core'
+    pod 'Firebase/Auth'
+    pod 'Firebase/Firestore'
 end
