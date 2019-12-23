35227e07a0a2cb444e2d3a15d7f9b65d752dd563
oualich <ouali.cherikh@gmail.Com>
2019-12-23 22:18:00 +0100

    Sign Up+ firebase

---
+++ MyEvents/Scenes/Events/ListEventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ListEventsViewController.swift
+//  MyEvents
+//
+//  Created by ouali on 23/12/2019.
+//  Copyright © 2019 Ouali Cherikh. All rights reserved.
+//
+
+import UIKit
+
+class ListEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
